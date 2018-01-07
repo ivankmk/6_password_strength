bd8a043444119946d57427cd8dda121ede79602f
ivankmk <kumeyko@hotmail.com>
2018-01-07 02:34:16 +0100

    script created

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+Script will check your password and revert strength rank:
+ 0 - highly weak password (in list of 500 worst passwords);
+ 10 - strong password;
+
+ # Requirements
+
+  - Python 3.5
+  - Packages: re, os, sys, urllib
+
+
+# How to launch
+
+  ```bash
+
+  $ python lang_frequency.py <password> # possibly requires call of python3 executive instead of just python
+  You password strength score 4 out of 10
+  ```
+
+  The same with Windows environment;
+
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,51 @@
+import re
+import os
+import sys
+import urllib.request
 
 
 def get_password_strength(password):
-    pass
+    if blacklist_check(password) == 0:
+        return 0
+    elif blacklist_check(password) is None:
+        return None
+    else:
+        return blacklist_check(password)+password_checker(password)
+
+
+def blacklist_check(password):
+    try:
+        url_address = ('https://raw.githubusercontent.com'
+        '/danielmiessler/SecLists/master/Passwords/500-worst-passwords.txt')
+        raw_file = urllib.request.urlopen(url_address).read().decode('utf-8')
+        blacklist = [line for line in raw_file.split('\n')]
+        if password in blacklist:
+            return 0
+        else:
+            return 1.6
+    except urllib.error.URLError:
+        return None
+
+
+def password_checker(password):
+        checks = ['[A-Z]', '[a-z]', '[^a-zA-Z0-9]', '[0-9]']
+        points = 0
+        for check in checks:
+            if bool(re.search(check, password)) is True:
+                points += 1.6
+            else:
+                continue
+        if len(password) > 14:
+            points += 1.6
+        else:
+            pass
+        return points
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) < 2:
+        sys.exit('No password provided')
+    else:
+        score = get_password_strength(sys.argv[1])
+        print('You password strength score {} out of 10.0'
+              .format(round(score, 0)))
